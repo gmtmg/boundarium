5088ee35537a64510db396ab79a9d0c1d2a165ad
gmtgm <164746559+gmtmg@users.noreply.github.com>
2024-05-01 12:58:39 +0900

    ボタンの撤廃
    直感的な操作のため、ボタンコンテナを撤廃
    停止機能と編集機能の統合

--- BoundArium_js/boundArium.js
+++ BoundArium_js/boundArium.js
@@ -1,7 +1,6 @@
 // boundArium.js
 
 const freq = {
-
   beep : {
     do1: 261.63, re1: 293.66, mi1: 329.63, fa1: 349.23, so1: 392.50, so_ra_1: 417, ra1: 440.00, shi1: 493.88, do2: 523.25
   },
@@ -14,9 +13,49 @@ const freq = {
     ra1  :"soundSource/ra1_piano.mp3",
     shi1 :"soundSource/shi1_piano.mp3",
     do2  :"soundSource/do2_piano.mp3",
+  },
+  darkpiano : {
+    do1  :"soundSource/do1_darkpiano.mp3",
+    re1  :"soundSource/re1_darkpiano.mp3",
+    mi1  :"soundSource/mi1_darkpiano.mp3",
+    fa1  :"soundSource/fa1_darkpiano.mp3",
+    so1  :"soundSource/so1_darkpiano.mp3",
+    ra1  :"soundSource/ra1_darkpiano.mp3",
+    shi1 :"soundSource/shi1_darkpiano.mp3",
+    do2  :"soundSource/do2_darkpiano.mp3",
+  },
+  
+  novelClav : {
+    do1  :"soundSource/do1_novelClav.mp3",
+    re1  :"soundSource/re1_novelClav.mp3",
+    mi1  :"soundSource/mi1_novelClav.mp3",
+    fa1  :"soundSource/fa1_novelClav.mp3",
+    so1  :"soundSource/so1_novelClav.mp3",
+    ra1  :"soundSource/ra1_novelClav.mp3",
+    shi1 :"soundSource/shi1_novelClav.mp3",
+    do2  :"soundSource/do2_novelClav.mp3",
+  },
+  vibp : {
+    do1  :"soundSource/do1_vibp.mp3",
+    re1  :"soundSource/re1_vibp.mp3",
+    mi1  :"soundSource/mi1_vibp.mp3",
+    fa1  :"soundSource/fa1_vibp.mp3",
+    so1  :"soundSource/so1_vibp.mp3",
+    ra1  :"soundSource/ra1_vibp.mp3",
+    shi1 :"soundSource/shi1_vibp.mp3",
+    do2  :"soundSource/do2_vibp.mp3",
+  },
+  drum : {
+    do1  :"soundSource/drum.mp3",
+    re1  :"soundSource/drum.mp3",
+    mi1  :"soundSource/drum.mp3",
+    fa1  :"soundSource/drum2.mp3",
+    so1  :"soundSource/drum2.mp3",
+    ra1  :"soundSource/drum2.mp3",
+    shi1 :"soundSource/drum3.mp3",
+    do2  :"soundSource/drum3.mp3",
   }
 };
-
 
 // Canvasの設定
 const canvas = document.getElementById('pinballCanvas');
@@ -58,18 +97,30 @@ function generateColorScheme() {
   var colorScheme = [
       'hsl(' + baseHue + ', ' + saturation + '%, ' + (lightness + 0.1) + '%)',
       'hsl(' + baseHue + ', ' + saturation + '%, ' + (lightness - 1) + '%)', // やや暗めの色
-      'hsl(' + (baseHue  + 10*mNum) + ', ' + (saturation+100*mNum) + '%, ' + (lightness + 80*mNum) + '%)',
+      'hsl(' + (baseHue  + 10*mNum) + ', ' + (saturation+20*mNum) + '%, ' + (lightness + 80*mNum) + '%)',
     
       'hsl(' + (baseHue  + 15*mNum) + ', ' + (saturation+20*mNum) + '%, ' + (lightness + 50*mNum) + '%)', // やや明るめの色
-      'hsl(' + (baseHue  + 30*mNum) + ', ' + (saturation+40*mNum) + '%, ' + (lightness + 70*mNum) + '%)',
+      'hsl(' + (baseHue  + 200*mNum) + ', ' + (saturation+40*mNum) + '%, ' + (lightness + 70*mNum) + '%)',
       'hsl(' + (baseHue  + 100*mNum) + ', ' + (saturation+100*mNum) + '%, ' + (lightness + 80*mNum) + '%)',
-      
+      'hsl(' + (baseHue  + 60*mNum) + ', ' + (saturation+30*mNum) + '%, ' + (lightness + 60*mNum) + '%)',
+      'hsl(' + (baseHue  + 200*mNum) + ', ' + (saturation+130*mNum) + '%, ' + (lightness + 30*mNum) + '%)',
+      'hsl(' + (baseHue  + 80*mNum) + ', ' + (saturation+30*mNum) + '%, ' + (lightness + 100*mNum) + '%)',
   ];
 
   return colorScheme;
 }
 function randomPos() {
   return Math.floor(Math.random() * (9)) -4;
+}
+function getRandomNonZero() {
+  let num = 0;
+  while (num === 0) {
+    num = Math.floor(Math.random() * 9) - 4;
+  }
+  return num;
+}
+function randomfreq() {
+  return Math.floor(Math.random() * (8));
 }
 colorList = generateColorScheme()
 document.body.style.backgroundColor = colorList[0];
@@ -98,7 +149,6 @@ function rgbToHsv(r, g, b) {
       }
       h /= 6;
   }
-
   return [h, s, v];
 }
 
@@ -160,11 +210,14 @@ var adjustedColor = adjustColorBrightness(canvasColor);
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 // 簡単なビープ音を鳴らす関数
 function playSound(soundType, gainN) {
+  gainN = gainN / 8;
+  if (gainN >= 1){
+    gainN = 1;
+  }
   if(!isNaN(soundType)){
     const oscillator = audioCtx.createOscillator();
     const gainNode = audioCtx.createGain();
     // 音量の設定 (1.0が標準の音量)
-    gainN = gainN / 4
     console.log(Math.abs(gainN));
     gainNode.gain.value = Math.abs(gainN);
     oscillator.type = "square"; // 音の形状
@@ -178,7 +231,7 @@ function playSound(soundType, gainN) {
     let source = audioCtx.createBufferSource();
     const gainNode = audioCtx.createGain();
     // 音量の設定 (1.0が標準の音量)
-    gainN = gainN / 4
+
     console.log(Math.abs(gainN));
     gainNode.gain.value = Math.abs(gainN);
     let request = new XMLHttpRequest();
@@ -204,50 +257,28 @@ function playSound(soundType, gainN) {
 // 296 : 4
 // squareSize - vx = size
 //サイズの定義  squareSize - (squareSize - size / x )
-let pinballs = [
-  {
-    id:0,
-    x: randomPos() ,
-    y: randomPos() ,
-    size: 48,
-    vx: randomPos(), // X方向の速度
-    vy: randomPos(), // Y方向の速度
-    soundTop: freq.beep.do1,
-    soundRight: freq.beep.fa1,
-    soundBottom: freq.beep.do2,
-    soundLeft: freq.beep.so1,
-    color: colorList[3],
-    radiusNum: 5
-  },  {
-    id:0,
-    x: randomPos() ,
-    y: randomPos() ,
-    size: 48,
-    vx: randomPos(), // X方向の速度
-    vy: randomPos(), // Y方向の速度
-    soundTop: freq.beep.re1,
-    soundRight: freq.beep.fa1,
-    soundBottom: freq.beep.re1,
-    soundLeft: freq.beep.so1,
-    color: colorList[4],
-    radiusNum: 5
-  },  {
-    id:0,
-    x: randomPos() ,
-    y: randomPos() ,
-    size: 48,
-    vx: randomPos(), // X方向の速度
-    vy: randomPos(), // Y方向の速度
-    soundTop: freq.beep.ra1,
-    soundRight: freq.beep.re1,
-    soundBottom: freq.beep.do2,
-    soundLeft: freq.beep.so1,
-    color: colorList[5],
-    radiusNum: 5
-  }
-];
+let pinballs = [];
+let pinballLength = Math.floor(Math.random() * 5) + 1;
+for (let i = 0; i < pinballLength; i++) {
+  pinballs.push(
+    {
+      id:i,
+      x: -4,
+      y: 0,
+      size: 48,
+      vx: randomPos(), // X方向の速度
+      vy: randomPos(), // Y方向の速度
+      soundTop: Object.values(freq.piano)[randomfreq()],
+      soundRight: Object.values(freq.piano)[randomfreq()],
+      soundBottom: Object.values(freq.piano)[randomfreq()],
+      soundLeft: Object.values(freq.piano)[randomfreq()],
+      color: colorList[i+3],
+      radiusNum: 5
+    }
+  )
+}
 
-const set_pinballs = pinballs;
+
 
 // sizeとvの値から初期位置を再設定
 function ballPosition(xy, size) {
@@ -258,15 +289,20 @@ function ballPosition(xy, size) {
 }
 
 pinballs.forEach(ball => {
+  if (ball.x == 4 || ball.x == -4)ball.vx = getRandomNonZero();
   if (ball.x == -4 && ball.vx > 0) ball.vx *= -1;
   if (ball.x == 4 && ball.vx < 0) ball.vx *= -1;
   ball.x = ballPosition(ball.x, ball.size);
+  if (ball.y == 4 || ball.y == -4)ball.vy = getRandomNonZero();
   if (ball.y == -4 && ball.vy > 0) ball.vy *= -1;
   if (ball.y == 4 && ball.vy < 0) ball.vy *= -1;
   ball.y = ballPosition(ball.y, ball.size);
+  
 });
 
-
+const setPinballs = JSON.parse(JSON.stringify(pinballs));
+const rectPinball = JSON.parse(JSON.stringify(pinballs));
+let prerectPinball = JSON.parse(JSON.stringify(rectPinball));
 function drawRoundedSquare(ctx, x, y, size, radius) {
   ctx.beginPath();
   ctx.moveTo(x + radius, y);
@@ -295,7 +331,7 @@ function drawSquare() {
 // ピンボールの描画と更新
 
   // 角丸の四角形を描く関数
-function drawRoundedRect(ctx, x, y, width, height, radius) {
+function drawRoundedRect(ctx, x, y, width, height, radius, styleStr) {
   ctx.beginPath();
   ctx.moveTo(x + radius, y);
   ctx.lineTo(x + width - radius, y);
@@ -307,7 +343,13 @@ function drawRoundedRect(ctx, x, y, width, height, radius) {
   ctx.lineTo(x, y + radius);
   ctx.quadraticCurveTo(x, y, x + radius, y);
   ctx.closePath();
-  ctx.fill();
+  if(styleStr == "fill") {
+    ctx.fill()
+  } else{
+  ctx.strokeStyle = colorList[2];
+  ctx.setLineDash([5, 5]); // 5ピクセル描画して15ピクセル空ける
+  ctx.stroke();
+  }
 }
 
 function slideAndFade(colorS, xS, speedS, cvs, con) {
@@ -348,7 +390,6 @@ function slideAndFadeHorizontal(colorS, yS, speedS, cvs, con) {
   draw();
 }
 
-
 function updatePinballs() {
   pinballs.forEach(pinball => {
     // ピンボールを描画
@@ -356,7 +397,6 @@ function updatePinballs() {
     if (pinball.x <= squareX + pinball.size / 2 ){
       pinball.vx *= -1; // X方向の速度の符号を反転
       playSound(pinball.soundLeft, pinball.vx);
-      // ここにエフェクトの関数を作りたい
       slideAndFade(pinball.color, 280, pinball.vx, ctxefW, effeCanvasW);
 
     }
@@ -387,7 +427,7 @@ function updatePinballs() {
 
   ctx.shadowBlur = 2; // 影のぼかし量
   //ctx.shadowColor = adjustedColor; // 影の色
-  drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum); // ここでの10は角の丸みの半径
+  drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "fill"); // ここでの10は角の丸みの半径
   });
 }
 
@@ -435,7 +475,7 @@ function createButton(pinball) {
   editNum = pinball.id
   editCtx.clearRect(0, 0, editCanvas.width, editCanvas.height);
   editCtx.fillStyle = pinball.color;
-  drawRoundedRect(editCtx, editCanvas.width / 2 - pinball.size / 2, editCanvas.height / 2 - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum);
+  drawRoundedRect(editCtx, editCanvas.width / 2 - pinball.size / 2, editCanvas.height / 2 - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "fill");
   }
 
 //canvas.addEventListener('click', function(e) {
@@ -461,19 +501,222 @@ playButton.addEventListener('click', function() {
   playSwith();
 });
 
+const editSquareContainer = document.getElementById('editSquareContainer'); 
+let edittingNum = 0;
+
 editButton.addEventListener('click', function() {
   editFlag = !editFlag;
   if(!editFlag){
+    editSquareContainer.innerHTML = '';
     playSwith();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ballRectSet(setPinballs, 0.3);
+    pinballs = JSON.parse(JSON.stringify(setPinballs));
+    allBln = true;
+    changeBln = false;
+    prerectPinball = JSON.parse(JSON.stringify(rectPinball));
+    arrowAllRect();
+
+    console.log('編集ボタンが押された');
+    function addSquare(pinball, delay){
+      setTimeout(() => {
+        const square = document.createElement('button');
+        square.style.width = pinball.size / 4 * 3 + 'px';
+        square.style.height = pinball.size / 4 * 3 + 'px';
+        square.style.backgroundColor = pinball.color;
+        square.style.borderRadius = pinball.radiusNum / 4 * 3 +  'px';
+        if(pinball.id == edittingNum){
+          square.style.opacity = '1';
+        }else{
+          square.style.opacity = '0.5';
+        }
+        
+        // スタイルを設定
+        square.classList.add('editSquare');
+        square.id = 'editSquare_' + pinball.id;
+        square.addEventListener('mouseover', () => {
+          rippleEffect(pinball.id);
+        });
+        square.addEventListener('click', function() {
+          editPinball(pinball.id); // 編集関数を呼び出す
+        });
+
+        // 対象の div に追加
+        editSquareContainer.appendChild(square);
+      }, delay);
+    }
+    let dlNum = 0;
+    pinballs.forEach(pinball => {
+      addSquare(pinball, dlNum);
+      dlNum += 100;
+    });
     editButton.classList.add('playing');
   }else{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ballRectSet(pinballs, 1);
     editButton.classList.remove('playing');
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
-    ctx.shadowBlur = 0;
-    ctx.shadowColor = 'transparent';
+    ctx.globalAlpha = 1;
+    function fadeAndSlideOut() {
+      const children = editSquareContainer.children;
+      
+      // 各要素にアニメーションを適用
+      for (let i = 0; i < children.length; i++) {
+        setTimeout(() => {
+          children[i].style.animation = 'fadeSlideOut 0.5s forwards';
+        }, i * 100); // 0.5秒ごとに次の要素にアニメーションを適用
+      }
+      return 100
+    }
     playSwith();
+    waitNum = fadeAndSlideOut()
+    setTimeout(() => {editSquareContainer.innerHTML = ''},waitNum);
   }
+});
+
+
+
+
+// 角丸正方形を描画する関数
+function drawRoundedEffectSquare(ctx, x, y, width, height, radius) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+}
+
+
+function rippleEffect(idNum) {
+  const pinball = setPinballs.find(p => p.id === idNum);
+  editPinball(pinball.id)
+  if (!arrowFlag){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ballRectSet(setPinballs, 0.3);
+    arrowFlag = false ;
+
+    //rippleFlag = false;
+    
+    if (!pinball) return;
+    let size = 0; // 波紋の初期サイズ
+    const maxSize = pinball.size*2; // 波紋の最大サイズ
+    const rippleSpeed = 3; // 波紋が広がる速度
+    const lineWidth = 2; // 波紋の線の太さ
+
+    function drawRipple() {
+      //ctx.globalAlpha = 0.3; // 波紋の透明度
+      ctx.beginPath();
+      drawRoundedEffectSquare(ctx, pinball.x - pinball.size/2, pinball.y - pinball.size/2, pinball.size, pinball.size, pinball.size/10);
+      ctx.fillStyle = "#ffffff"; // 波紋の色
+      ctx.strokeStyle = "#ffffff"; 
+      ctx.lineWidth = lineWidth;
+      ctx.stroke();
+      size += rippleSpeed;
+      if (size < maxSize) {
+        requestAnimationFrame(drawRipple);
+      }
+    }
+    drawRipple();
+    }  
+  }
+let arrowFlag =false;
+
+function arrowAnimation (pinball){
+  if (changeBln) return;
+  if (pinball.x <= squareX + pinball.size / 2 ){
+    pinball.vx *= -1; // X方向の速度の符号を反転
+    playSound(pinball.soundLeft, pinball.vx);
+    slideAndFade(pinball.color, 280, pinball.vx, ctxefW, effeCanvasW);
+    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "dash");
+  }
+  if (pinball.x >= squareX + squareSize - pinball.size / 2){
+    pinball.vx *= -1; // X方向の速度の符号を反転
+    playSound(pinball.soundRight, pinball.vx);
+    slideAndFade(pinball.color, 0, pinball.vx, ctxefE, effeCanvasE); 
+    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "dash");
+  }
+  // 上か下の壁に当たった場合
+  if (pinball.y <= squareY + pinball.size / 2){
+    pinball.vy *= -1; // Y方向の速度の符号を反転
+    playSound(pinball.soundTop, pinball.vy);
+    slideAndFadeHorizontal(pinball.color, 140, pinball.vy, ctxefN, effeCanvasN); 
+    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "dash");
+  }
+  if (pinball.y >= squareY + squareSize - pinball.size / 2){
+    pinball.vy *= -1; // Y方向の速度の符号を反転
+    playSound(pinball.soundBottom, pinball.vy);
+    slideAndFadeHorizontal(pinball.color, 0, pinball.vy, ctxefS, effeCanvasS);
+    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "dash");
+  }
+  pinball.x += pinball.vx;
+  pinball.y += pinball.vy;
+  ctx.fillStyle = pinball.color;
+  drawRoundedRect(ctx, pinball.x - pinball.size / 80, pinball.y - pinball.size / 80, pinball.size / 80, pinball.size/ 80, pinball.radiusNum, "fill");
+  }
+let animID;
+function arrowAllRect() {
+  if(editFlag) return;
+  if(!allBln) return;
+  prerectPinball.forEach(pinball => {
+    arrowAnimation(pinball);
+    
+  });
+  animID = requestAnimationFrame(arrowAllRect);
+}
+
+
+function arrowRect() {
+  //arrowFlag = false;
+  if(editFlag) return;
+  
+  prerectPinball.forEach(pinball => {
+    if(pinball.id == edittingNum){
+      arrowAnimation(pinball);
+    }
+  });
+  changeBln = !changeBln;
+  animID = requestAnimationFrame(arrowRect);
+}
+let allBln = true;
+let changeBln = false;
+function editPinball(idNum) {
+  document.querySelectorAll('.editSquare').forEach(button => {
+    button.style.opacity = '0.5';
+  });
+  cancelAnimationFrame(animID);
+  // クリックされたボタンの透明度を1に設定
+  const selectedButton = document.getElementById('editSquare_' + idNum);
+  if (selectedButton) {
+    arrowFlag = true ;
+    selectedButton.style.opacity = '1';
+    prerectPinball = JSON.parse(JSON.stringify(rectPinball));
+    allBln = false;
+    changeBln = true;
+    
+    arrowRect();
+    
+  }
+  edittingNum = idNum;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ballRectSet(setPinballs, 0.3);
+}
+
+
+
+
+
+
+
+
+
+
+resetButton.addEventListener('click', function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // 画面をクリア
 });
 
 pianotile.addEventListener('click', function() {
@@ -488,14 +731,19 @@ pianotile.addEventListener('click', function() {
 
 drawSquare();
 
-function ballRectSet(pinballsData){
+function ballRectSet(pinballsData, alpha){
   pinballsData.forEach(pinball => {
     ctx.fillStyle = pinball.color;
+    if (pinball.id == edittingNum){
+      ctx.globalAlpha = 1;
+    }else{
+      ctx.globalAlpha = alpha;
+    }
     //ctx.shadowOffsetX = -(pinball.x - squareSize / 2) / shadowOffLen; // 影のX方向のオフセット
     //ctx.shadowOffsetY = -(pinball.y - squareSize / 2) / shadowOffLen; // 影のY方向のオフセット
     ctx.shadowBlur = 4; // 影のぼかし量
     ctx.shadowColor = adjustedColor; 
-    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum); // ここでの10は角の丸みの半径
+    drawRoundedRect(ctx, pinball.x - pinball.size / 2, pinball.y - pinball.size / 2, pinball.size, pinball.size, pinball.radiusNum, "fill"); // ここでの10は角の丸みの半径
   });
 }
 // アニメーションループ
@@ -503,12 +751,13 @@ function animate() {
   if (!isPlaying) return;
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 画面をクリア
   //drawSquare(); // 枠線を再描画
+  //ctx.globalAlpha = 0.5;
   updatePinballs(); // ピンボールを更新
   requestAnimationFrame(animate); // 次のフレームを要求
 }
 
 
-ballRectSet(pinballs);
+ballRectSet(pinballs, 1);
 animate();
 
 
